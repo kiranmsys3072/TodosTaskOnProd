2c4e4c345d6d07e1aeb0512b6ab13aff244b0d8e
Kelangi Raj Kiran <kkiran@msystechnologies.com>
2023-07-14 16:08:37 +0530

    installed dotenv paclage

--- NODE-TASK/index.js
+++ NODE-TASK/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const todoRoutes = require("./routes/todoRoutes");
 const connection = require("./db/connection");
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
 const PORT = process.env.PORT || 3030;
 const morgan = require("morgan");
